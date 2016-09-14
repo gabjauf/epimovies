9b9cfe1ff95de844492a4324cd3e83b6deb4b5c5
Alexandra Lucas <alexandra.lucas@epita.fr>
2016-09-14 12:32:58 +0000

    [M][JS] move content in app.js

--- app.js
+++ app.js
@@ -26,3 +26,12 @@ app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
+
+var allocine = require("allocine-api");
+
+allocine.api("search", {q: "spiderman", filter: "movie"}, function(error, results) {
+    if(error) { console.log("Error : "+ error); return; }
+
+    alert("Voici les données retournées par l'API Allociné:");
+    alert(results);
+});
