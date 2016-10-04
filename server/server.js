6fa95e2b40664eb53289e51aad8b094c80b98c5b
Gabriel Jauffret <jauffr_a@epita.fr>
2016-10-04 14:23:04 +0200

    Add basic server part

---
+++ server/server.js
@@ -0,0 +1,17 @@
+"use strict";
+var express = require('express');
+var path = require('path');
+var port = process.env.PORT || 3000;
+var app = express();
+app.use('/app', express.static(path.resolve(__dirname, 'app')));
+app.use('/libs', express.static(path.resolve(__dirname, 'libs')));
+var renderIndex = function (req, res) {
+    res.sendFile(path.resolve(__dirname, 'index.html'));
+};
+app.get('/*', renderIndex);
+var server = app.listen(port, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('This express app is listening on port:' + port);
+});
+//# sourceMappingURL=server.js.map
