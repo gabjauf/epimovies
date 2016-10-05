3a9d2901fc54f1d4946acdc12bd542076f4d5b87
Alexandra LUCAS - lucas_i <alexandra.lucas@epita.fr>
2016-10-05 18:50:54 +0200

    [C][CODE] clean code

--- public/js/hello_world_controller.js
+++ public/js/hello_world_controller.js
@@ -1,12 +1,17 @@
 var app = angular.module('App', []);
 
-var key = "37a090ff916d7012771f3df0cbc986f5";
-var head = "https://api.themoviedb.org/3/discover/";
+var key = "api_key=37a090ff916d7012771f3df0cbc986f5";
+var head = "https://api.themoviedb.org/3/discover/movie?";
+var primary_release_date = "primary_release_date.";
+var sup = "gt";
+var inf = "lt";
+var equal = "e";
+var and = "&";
 
 app.controller('Controller', function($scope, $http){
     $http({
         methode : "GET",
-        url : head + "movie?primary_release_date.gte=2016-09-20&primary_release_date.lte=2016-10-04&api_key=" + key
+        url : head + primary_release_date + sup + equal + "=2016-09-20" + and + primary_release_date + inf + equal + "=2016-10-04" + and + key
     }).then(function mySuccess(response) {
         $scope.greeting = response.data;
     }, function myError(response) {
