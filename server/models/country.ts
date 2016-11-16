e4b5da798e20c144d36b59cc0b9bcbf55d4e8bc1
Gabriel Jauffret <jauffr_a@epita.fr>
2016-11-16 18:09:34 +0100

    Update country Model

--- server/models/country.ts
+++ server/models/country.ts
@@ -2,4 +2,11 @@ export class Country {
 
     _name : string;
 
+        constructor(name : string) {
+        this._name = name;
+    }
+
+    getCountry() {
+        return {"name" : this._name};
+    }
 }
