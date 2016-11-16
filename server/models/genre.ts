f87cc9e7601191074157a1789bf14664856a684f
Gabriel Jauffret <jauffr_a@epita.fr>
2016-11-16 18:05:17 +0100

    add the GET method for each model already present

--- server/models/genre.ts
+++ server/models/genre.ts
@@ -1,4 +1,12 @@
 export class Genre {
     
     _name : string;
+
+    constructor(name : string) {
+        this._name = name;
+    }
+
+    getGenre() {
+        return {"name" : this._name};
+    }
 }
--- server/models/person.ts
+++ server/models/person.ts
@@ -1,21 +1,12 @@
 export class Person {
 
-    //_firstname : string;
-    //_lastname : string;
-    //_birthDate : string;
     _name : string;
 
-    constructor(//firstname : string,
-                //lastname : string,
-                //birthDate  : string
-                name : string) {
-        
-        //this._firstname = firstname;
-        //this._lastname = lastname;
-        //this._birthDate = birthDate;
+    constructor(name : string) {
         this._name = name;
     }
 
-    
-
+    getPerson() {
+        return {"name" : this._name};
+    }
 } 
