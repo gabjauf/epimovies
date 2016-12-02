d62c8e106c7325c23d818d4f95f81ea82c7d9d1d
Gabriel Jauffret <jauffr_a@epita.fr>
2016-12-02 16:49:26 +0100

    ADD database schema for mysql

---
+++ schema.sql
@@ -0,0 +1,195 @@
+-- MySQL dump 10.13  Distrib 5.7.16, for Linux (x86_64)
+--
+-- Host: localhost    Database: epimovies
+-- ------------------------------------------------------
+-- Server version	5.7.16-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `t_countries`
+--
+
+DROP TABLE IF EXISTS `t_countries`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_countries` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_uniq` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=13932 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `t_genres`
+--
+
+DROP TABLE IF EXISTS `t_genres`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_genres` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_uniq` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=23729 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `t_movie-country`
+--
+
+DROP TABLE IF EXISTS `t_movie-country`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_movie-country` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_movie` int(11) NOT NULL,
+  `id_country` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `u_Mov_Coun` (`id_movie`,`id_country`),
+  KEY `fk_mc_country_idx` (`id_country`),
+  KEY `fk_mc_movie_idx` (`id_movie`),
+  CONSTRAINT `fk_mc_country` FOREIGN KEY (`id_country`) REFERENCES `t_countries` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_mc_movie` FOREIGN KEY (`id_movie`) REFERENCES `t_movies` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=4131 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `t_movie-genre`
+--
+
+DROP TABLE IF EXISTS `t_movie-genre`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_movie-genre` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_movie` int(11) NOT NULL,
+  `id_genre` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `u_Mov_Gen` (`id_movie`,`id_genre`),
+  KEY `id_movie_idx` (`id_movie`),
+  KEY `id_genre_idx` (`id_genre`),
+  CONSTRAINT `fk_genre` FOREIGN KEY (`id_genre`) REFERENCES `t_genres` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_movie` FOREIGN KEY (`id_movie`) REFERENCES `t_movies` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=6555 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `t_movies`
+--
+
+DROP TABLE IF EXISTS `t_movies`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_movies` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(128) NOT NULL,
+  `plot` varchar(512) DEFAULT NULL,
+  `release_date` date DEFAULT NULL,
+  `language` varchar(128) DEFAULT NULL,
+  `poster` varchar(256) DEFAULT NULL,
+  `awards` varchar(256) DEFAULT NULL,
+  `imdbRating` float DEFAULT NULL,
+  `imdbId` varchar(45) DEFAULT NULL,
+  `type` varchar(45) DEFAULT NULL,
+  `year` int(11) DEFAULT NULL,
+  `metascore` int(11) DEFAULT NULL,
+  `runtime` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`,`title`),
+  UNIQUE KEY `title_UNIQUE` (`title`),
+  UNIQUE KEY `imdbId_UNIQUE` (`imdbId`)
+) ENGINE=InnoDB AUTO_INCREMENT=33699 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `t_person`
+--
+
+DROP TABLE IF EXISTS `t_person`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_person` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=77597 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `t_rating`
+--
+
+DROP TABLE IF EXISTS `t_rating`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_rating` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
+  `id_movie` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNQ_user_movie` (`id_user`,`id_movie`),
+  KEY `fk_movie_rating_idx` (`id_movie`),
+  CONSTRAINT `fk_movie_rating` FOREIGN KEY (`id_movie`) REFERENCES `t_movies` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_rating` FOREIGN KEY (`id_user`) REFERENCES `t_user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `t_role`
+--
+
+DROP TABLE IF EXISTS `t_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_movie` int(11) NOT NULL,
+  `id_person` int(11) NOT NULL,
+  `role` varchar(128) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniqueRole` (`id_movie`,`id_person`,`role`),
+  KEY `id_movie_idx` (`id_movie`),
+  KEY `id_person_idx` (`id_person`),
+  CONSTRAINT `fk_id_movie` FOREIGN KEY (`id_movie`) REFERENCES `t_movies` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_id_person` FOREIGN KEY (`id_person`) REFERENCES `t_person` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=21980 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `t_user`
+--
+
+DROP TABLE IF EXISTS `t_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-12-02 16:48:05
