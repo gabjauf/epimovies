f13f7f1360d3fe8da0fba8b558a9aef9c102c162
lucas_i <alexandra.lucas@epita.fr>
2016-12-13 17:08:21 +0100

    [RATING] try to add post /rating

--- public/js/epimovies.js
+++ public/js/epimovies.js
@@ -1,16 +1,38 @@
 var app = angular.module('Epimovies', []);
 
-var head = "http://localhost:3000/api/movie/getAll";
+var head = "http://localhost:3000/api";
 
 app.controller('Movies', function($scope, $http){
     $http({
         methode : "GET",
-        url : head
+        url : head + "/movie/getAll"
     }).then(function mySuccess(response) {
         $scope.res = response.data;
     }, function myError(response) {
         $scope.res = response.statusText;
     });
+});
+
+app.controller('Like', function ($scope, $http) {
+    $scope.SendData = function (movieId, userId) {
+        var data = $.param({
+            movieId: movieId,
+            userId: userId
+        })
+
+        $http({
+            methode: "POST",
+            url: head + "/rating",
+            data: data,
+            headers: "Content-Type': 'application/x-www-form-urlencoded"
+        }).success(function (data, status, headers) {
+            $scope.PostDataResponse = data;
+        }).error(function (data, status, header) {
+            $scope.ResponseDetails = "Data: " + data +
+                "<hr />status: " + status +
+                "<hr />headers: " + header;
+        });
+    }
 });
 
 app.controller('Recommendations', function($scope, $http){
